3f902bde786c0c954dd39b404508cc3fb42ee684
saraivaa.me <wesley-saraiva@outlook.com>
2026-05-01 18:51:00 -0300

    style: hero e navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,36 +1,49 @@
 import PLACASHERO from "../assets/images/PLACAS-HERO.png";
 import BUGGYCASALHERO from "../assets/images/BUGGY-CASAL-HERO.png";
+import { MoveUpRight } from "lucide-react";
 
 export function Home() {
   return (
-    <section className=" relative overflow-hidden bg-[url('./assets/images/PRAIA.png')] bg-cover bg-center rounded-b-7xl rounded-b-[60px] shadow-2xl">
+    <section className="relative overflow-hidden bg-[url('./assets/images/PRAIA.png')] bg-cover bg-center rounded-b-7xl rounded-b-[60px] shadow-2xl">
       <div className="container mx-auto px-4 py-8 h-162.5 md:h-187.5 relative">
-        <div className="absolute -left-40 bottom-0 w-75 z-10">
+        <div className="fixed md:w-90 -left-40 top-30 w-75 z-10 rotate-3">
           <img src={PLACASHERO} alt=" buggy Jericoacoara" />
         </div>
-        <div>
-          <div className="relative z-20 flex h-full items-center">
-            <div className="max-w-xl">
-              <h1 className="text-4xl font-bold text-white mb-4">
-                Descubra Jericoacoara com quem é da casa.
+        <div className="relative top-15 z-20 flex h-full items-center">
+          <div className="max-w-2xl ml-2 md:ml-30">
+            <div className="rounded-3xl p-8 md:p-10 backdrop-blur-sm border border-white/20 shadow-2xl">
+              <h1 className="text-4xl md:text-6xl font-bold text-white leading-tight">
+                Descubra{" "}
+                <span className="font-caesar-regular">Jericoacoara</span>
+                <br></br>com quem é da casa.
               </h1>
-              <p>
-                Jerilifetur é uma empresa 100% nativa, formada por guias locais
-                apaixonados por Jeri. Sabemos que você planeja a viagem durante
-                meses ou anos e, por isso, fazemos cada segundo valer a pena.
-                Nosso compromisso é com pontualidade, responsabilidade,
-                assistência e segurança.
+
+              <p className="mt-5 text-white/90 text-lg leading-relaxed max-w-xl">
+                A Jerilifetur é uma empresa 100% nativa, formada por guias
+                locais apaixonados por Jeri. Transformamos sua viagem em uma
+                experiência inesquecível com segurança, pontualidade e
+                atendimento real.
               </p>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
-                Agendar Paaseio
+            </div>
+            <div className="mt-8 flex flex-wrap md:flex-nowrap gap-4">
+              <button className="group inline-flex items-center gap-3 rounded-full bg-white px-2 py-2 shadow-xl transition-all duration-300 hover:scale-105">
+                <span className="pl-4 text-black font-alan-sans-medium">
+                  Agendar Passeio
+                </span>
+
+                <div className="grid h-10 w-10 place-items-center rounded-full bg-custom-blue-light text-white">
+                  <MoveUpRight size={20} />
+                </div>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded mt-4 ml-2">
-                Marcar Kite Trip
+
+              <button className="inline-flex items-center gap-3 rounded-full border border-white px-5 py-2 shadow-xl font-alan-sans-medium text-white">
+                Agendar Passeio
+                <MoveUpRight size={20} />
               </button>
             </div>
           </div>
         </div>
-        <div className="absolute -right-80 -bottom-5 w-300 z-100">
+        <div className=" md:w-300 -right-80 -bottom-5 w-300 z-100 hidden md:absolute md:block">
           <img
             src={BUGGYCASALHERO}
             alt="Colorful buggy for sale in Jericoacoara"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,9 +16,9 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="fixed inset-x-0 top-6 z-50 px-4">
+    <header className="fixed inset-x-20 top-6 z-50 px-4">
       <nav className=" md:rounded-full rounded-3xl bg-custom-white/95 shadow-2xl backdrop-blur-md border border-slate-200">
-        <div className="container flex items-center justify-between py-4">
+        <div className="flex items-center justify-between px-6 py-2">
           <div>
             <a href="/">
               <img src={logo} alt="Logo" className="w-35 md:w-40" />
@@ -31,14 +31,15 @@ export default function Navbar() {
               ))}
             </ul>
           </div>
-          <div className="flex items-center gap-6">
-            <button className="flex px-2 py-1 overflow-hidden rounded-full bg-[#F5F5F5]">
-              <span className="md:px-5 px-3 py-2 font-alan-sans-medium text-sm text-custom-blue-light ">
+          <div className=" items-center gap-6 hidden md:flex">
+            <button className="group inline-flex items-center gap-3 rounded-full bg-white px-2 py-2 shadow-xl transition-all duration-300 hover:scale-105">
+              <span className="pl-4 text-black font-alan-sans-medium">
                 Agendar Passeio
               </span>
-              <span className="flex items-center justify-center bg-custom-blue-light px-2.5 rounded-full">
+
+              <div className="grid h-10 w-10 place-items-center rounded-full bg-custom-blue-light text-white">
                 <CalendarDays size={20} />
-              </span>
+              </div>
             </button>
           </div>
           <div className="md:hidden" onClick={() => setOpen(!open)}>
